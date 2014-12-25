989a35f5b3a4703f27b6121dffaf06e1dcd49f82
gnishida <gnishida@purdue.edu>
2014-12-25 10:50:29 -0500

    Add brushfire2
    - swap zone between two adjacent cells.

---
+++ Brushfire2/main.cpp
@@ -0,0 +1,356 @@
+﻿/**
+ * Brushfireアルゴリズムを使って、距離マップを計算する。
+ * 住宅ゾーンに加えて、５種類のゾーンタイプがあり、各セルについて、直近の各種類のゾーンまでの距離を計算する。
+ * GPU版も実装していたが、キューの実装においてcritical sectionが必要であるものの、CUDAでは実装できないことが判明。
+ * CPU版で十分速いので、CPU版で良しとしよう。
+ *
+ * 隣のセルと、ゾーンタイプを交換する
+ *
+ * @author Gen Nishida
+ * @date 12/24/2014
+ * @version 1.1
+ */
+
+#include <list>
+#include <vector>
+#include <time.h>
+
+#define CITY_SIZE 10 //200
+#define MAX_DIST 99
+#define BF_CLEARED -1
+#define MAX_ITERATIONS 1000
+#define NUM_FEATURES 5
+
+int bf_count = 0;
+
+struct Point2D {
+	int x;
+	int y;
+};
+
+struct BF_QueueElement {
+	int pos;
+	int type;
+
+	BF_QueueElement(int pos, int type): pos(pos), type(type) {}
+};
+
+unsigned int rand(unsigned int* randx) {
+    *randx = *randx * 1103515245 + 12345;
+    return (*randx)&2147483647;
+}
+
+float randf(unsigned int* randx) {
+	return rand(randx) / (float(2147483647) + 1);
+}
+
+float randf(unsigned int* randx, float a, float b) {
+	return randf(randx) * (b - a) + a;
+}
+
+int sampleFromCdf(unsigned int* randx, float* cdf, int num) {
+	float rnd = randf(randx, 0, cdf[num-1]);
+
+	for (int i = 0; i < num; ++i) {
+		if (rnd <= cdf[i]) return i;
+	}
+
+	return num - 1;
+}
+
+int sampleFromPdf(unsigned int* randx, float* pdf, int num) {
+	if (num == 0) return 0;
+
+	float cdf[40];
+	cdf[0] = pdf[0];
+	for (int i = 1; i < num; ++i) {
+		if (pdf[i] >= 0) {
+			cdf[i] = cdf[i - 1] + pdf[i];
+		} else {
+			cdf[i] = cdf[i - 1];
+		}
+	}
+
+	return sampleFromCdf(randx, cdf, num);
+}
+
+void dumpZone(int* zone) {
+	printf("<<< Zone Map >>>\n");
+	for (int r = 0; r < CITY_SIZE; ++r) {
+		for (int c = 0; c < CITY_SIZE; ++c) {
+			printf("%2d ", zone[r * CITY_SIZE + c]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+}
+
+void dumpDist(int* dist, int featureId) {
+	printf("<<< Distance Map (featureId = %d) >>>\n", featureId);
+	for (int r = 0; r < CITY_SIZE; ++r) {
+		for (int c = 0; c < CITY_SIZE; ++c) {
+			printf("%2d ", dist[(r * CITY_SIZE + c) * NUM_FEATURES + featureId]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+}
+
+inline bool isOcc(int* obst, int s, int featureId) {
+	return obst[s * NUM_FEATURES + featureId] == s;
+}
+
+inline int distance(int pos1, int pos2) {
+	int x1 = pos1 % CITY_SIZE;
+	int y1 = pos1 / CITY_SIZE;
+	int x2 = pos2 % CITY_SIZE;
+	int y2 = pos2 / CITY_SIZE;
+
+	return abs(x1 - x2) + abs(y1 - y2);
+}
+
+void clearCell(int* dist, int* obst, int s, int featureId) {
+	dist[s * NUM_FEATURES + featureId] = MAX_DIST;
+	obst[s * NUM_FEATURES + featureId] = BF_CLEARED;
+}
+
+void raise(std::list<std::pair<int, int> >& queue, int* dist, int* obst, bool* toRaise, int s, int featureId) {
+	Point2D adj[] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+	int x = s % CITY_SIZE;
+	int y = s / CITY_SIZE;
+
+	for (int i = 0; i < 4; ++i) {
+		int nx = x + adj[i].x;
+		int ny = y + adj[i].y;
+
+		if (nx < 0 || nx >= CITY_SIZE || ny < 0 || ny >= CITY_SIZE) continue;
+		int n = ny * CITY_SIZE + nx;
+
+		if (obst[n * NUM_FEATURES + featureId] != BF_CLEARED && !toRaise[n * NUM_FEATURES + featureId]) {
+			if (!isOcc(obst, obst[n * NUM_FEATURES + featureId], featureId)) {
+				clearCell(dist, obst, n, featureId);
+				toRaise[n * NUM_FEATURES + featureId] = true;
+			}
+			queue.push_back(std::make_pair(n, featureId));
+		}
+	}
+
+	toRaise[s * NUM_FEATURES + featureId] = false;
+}
+
+void lower(std::list<std::pair<int, int> >& queue, int* dist, int* obst, bool* toRaise, int s, int featureId) {
+	Point2D adj[] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+	int x = s % CITY_SIZE;
+	int y = s / CITY_SIZE;
+
+	for (int i = 0; i < 4; ++i) {
+		int nx = x + adj[i].x;
+		int ny = y + adj[i].y;
+
+		if (nx < 0 || nx >= CITY_SIZE || ny < 0 || ny >= CITY_SIZE) continue;
+		int n = ny * CITY_SIZE + nx;
+
+		if (!toRaise[n * NUM_FEATURES + featureId]) {
+			int d = distance(obst[s * NUM_FEATURES + featureId], n);
+			if (d < dist[n * NUM_FEATURES + featureId]) {
+				dist[n * NUM_FEATURES + featureId] = d;
+				obst[n * NUM_FEATURES + featureId] = obst[s * NUM_FEATURES + featureId];
+				queue.push_back(std::make_pair(n, featureId));
+			}
+		}
+	}
+}
+
+void updateDistanceMap(std::list<std::pair<int, int> >& queue, int* zone, int* dist, int* obst, bool* toRaise) {
+	while (!queue.empty()) {
+		std::pair<int, int> s = queue.front();
+		queue.pop_front();
+
+		if (toRaise[s.first * NUM_FEATURES + s.second]) {
+			raise(queue, dist, obst, toRaise, s.first, s.second);
+		} else if (isOcc(obst, obst[s.first * NUM_FEATURES + s.second], s.second)) {
+			lower(queue, dist, obst, toRaise, s.first, s.second);
+		}
+
+		bf_count++;
+	}
+}
+
+void setStore(std::list<std::pair<int, int> >& queue, int* zone, int* dist, int* obst, bool* toRaise, int s, int featureId) {
+	// put stores
+	obst[s * NUM_FEATURES + featureId] = s;
+	dist[s * NUM_FEATURES + featureId] = 0;
+
+	queue.push_back(std::make_pair(s, featureId));
+}
+
+void removeStore(std::list<std::pair<int, int> >& queue, int* zone, int* dist, int* obst, bool* toRaise, int s, int featureId) {
+	clearCell(dist, obst, s, featureId);
+
+	toRaise[s * NUM_FEATURES + featureId] = true;
+
+	queue.push_back(std::make_pair(s, featureId));
+}
+
+/**
+ * 計算したdistance mapが正しいか、チェックする。
+ */
+int check(int* zone, int* dist) {
+	int count = 0;
+
+	for (int r = 0; r < CITY_SIZE; ++r) {
+		for (int c = 0; c < CITY_SIZE; ++c) {
+			for (int k = 0; k < NUM_FEATURES; ++k) {
+				int min_dist = MAX_DIST;
+				for (int r2 = 0; r2 < CITY_SIZE; ++r2) {
+					for (int c2 = 0; c2 < CITY_SIZE; ++c2) {
+						if (zone[r2 * CITY_SIZE + c2] - 1 == k) {
+							int d = distance(r2 * CITY_SIZE + c2, r * CITY_SIZE + c);
+							if (d < min_dist) {
+								min_dist = d;
+							}
+						}
+					}
+				}
+
+				if (dist[(r * CITY_SIZE + c) * NUM_FEATURES + k] != min_dist) {
+					if (count == 0) {
+						printf("e.g. (%d, %d) featureId = %d\n", c, r, k);
+					}
+					count++;
+				}
+			}
+		}
+	}
+	
+	if (count > 0) {
+		printf("Check results: #error cells = %d\n", count);
+	}
+
+	return count;
+}
+
+/**
+ * ゾーンプランを生成する。
+ */
+void generateZoningPlan(int* zone, std::vector<float> zoneTypeDistribution) {
+	std::vector<float> numRemainings(NUM_FEATURES + 1);
+	for (int i = 0; i < NUM_FEATURES + 1; ++i) {
+		numRemainings[i] = CITY_SIZE * CITY_SIZE * zoneTypeDistribution[i];
+	}
+
+	unsigned int randx = 0;
+
+	for (int r = 0; r < CITY_SIZE; ++r) {
+		for (int c = 0; c < CITY_SIZE; ++c) {
+			int type = sampleFromPdf(&randx, numRemainings.data(), numRemainings.size());
+			zone[r * CITY_SIZE + c] = type;
+			numRemainings[type] -= 1;
+		}
+	}
+}
+
+int main() {
+	time_t start, end;
+
+	int* zone;
+	zone = (int*)malloc(sizeof(int) * CITY_SIZE * CITY_SIZE);
+	int* dist;
+	dist = (int*)malloc(sizeof(int) * CITY_SIZE * CITY_SIZE * NUM_FEATURES);
+	int* obst;
+	obst = (int*)malloc(sizeof(int) * CITY_SIZE * CITY_SIZE * NUM_FEATURES);
+	bool* toRaise;
+	toRaise = (bool*)malloc(CITY_SIZE * CITY_SIZE * NUM_FEATURES);
+	
+	// initialize the zone
+	std::vector<float> zoneTypeDistribution(6);
+	zoneTypeDistribution[0] = 0.5f; // 住宅
+	zoneTypeDistribution[1] = 0.2f; // 商業
+	zoneTypeDistribution[2] = 0.1f; // 工場
+	zoneTypeDistribution[3] = 0.1f; // 公園
+	zoneTypeDistribution[4] = 0.05f; // アミューズメント
+	zoneTypeDistribution[5] = 0.05f; // 学校・図書館
+
+	// 初期プランを生成
+	start = clock();
+	generateZoningPlan(zone, zoneTypeDistribution);
+	end = clock();
+	printf("generateZoningPlan: %lf\n", (double)(end-start)/CLOCKS_PER_SEC);
+
+	// キューのセットアップ
+	std::list<std::pair<int, int> > queue;
+	for (int i = 0; i < CITY_SIZE * CITY_SIZE; ++i) {
+		for (int k = 0; k < NUM_FEATURES; ++k) {
+			toRaise[i * NUM_FEATURES + k] = false;
+			if (zone[i] - 1 == k) {
+				setStore(queue, zone, dist, obst, toRaise, i, k);
+			} else {
+				dist[i * NUM_FEATURES + k] = MAX_DIST;
+				obst[i * NUM_FEATURES + k] = BF_CLEARED;
+			}
+		}
+	}
+
+	updateDistanceMap(queue, zone, dist, obst, toRaise);
+
+	//dumpZone(zone);
+	//dumpDist(dist, 4);
+	check(zone, dist);
+	
+	bf_count = 0;
+	int adj[4];
+	adj[0] = -1; adj[1] = 1; adj[2] = -CITY_SIZE; adj[3] = CITY_SIZE;
+	for (int iter = 0; iter < MAX_ITERATIONS; ++iter) {
+		queue.clear();
+
+		// ２つのセルのゾーンタイプを交換
+		int s1;
+		int s2;
+		while (true) {
+			s1 = rand() % (CITY_SIZE * CITY_SIZE);
+			int u = rand() % 4;
+			s2 = s1 + adj[u];
+
+			if (s2 < 0 || s2 >= CITY_SIZE * CITY_SIZE) continue;
+			if (zone[s1] == zone[s2]) continue;
+
+			int x1 = s1 % CITY_SIZE;
+			int y1 = s1 / CITY_SIZE;
+			int x2 = s2 % CITY_SIZE;
+			int y2 = s2 / CITY_SIZE;
+			if (abs(x1 - x2) + abs(y1 - y2) > 1) continue;
+
+			break;
+		}
+		
+		// move a store
+		int f1 = zone[s1] - 1;
+		int f2 = zone[s2] - 1;
+		zone[s1] = f2 + 1;
+		if (f1 >= 0) {
+			removeStore(queue, zone, dist, obst, toRaise, s1, f1);
+		}
+		if (f2 >= 0) {
+			setStore(queue, zone, dist, obst, toRaise, s1, f2);
+		}
+		zone[s2] = f1 + 1;
+		if (f2 >= 0) {
+			removeStore(queue, zone, dist, obst, toRaise, s2, f2);
+		}
+		if (f1 >= 0) {
+			setStore(queue, zone, dist, obst, toRaise, s2, f1);
+		}
+		updateDistanceMap(queue, zone, dist, obst, toRaise);
+		
+		//dumpZone(zone);
+		//dumpDist(dist, 4);
+		if (check(zone, dist) > 0) break;
+	}
+
+	printf("avg bf_count = %d\n", bf_count / MAX_ITERATIONS);
+	printf("total bf_count = %d\n", bf_count);
+
+	return 0;
+}
