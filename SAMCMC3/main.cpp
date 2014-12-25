90a660b824eb67302eac5aea43ce3a033b5791ab
gnishida <gnishida@purdue.edu>
2014-12-25 17:59:35 -0500

    add MCMC class.

---
+++ SAMCMC3/main.cpp
@@ -0,0 +1,16 @@
+﻿#include "MCMC.h"
+
+int main() {
+	int* zone;
+	int size;
+
+	MCMC mcmc;
+	mcmc.findBestPlan(&zone, &size);
+
+	mcmc.showZone(size, zone, "zone.png");
+	mcmc.saveZone(size, zone, "zone.txt");
+
+	free(zone);
+
+	return 0;
+}
